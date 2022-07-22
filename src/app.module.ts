c1af83c3922d22023fe1b0b9d87b580e14265001
jonathan <rodjon@eltiempo.com>
2022-07-22 09:25:44 -0500

    initial commit, fist project release

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RepositoryModule } from './repository/repository.module';
+import { CatsService } from './cats/cats.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    RepositoryModule,
+  ],
+  providers: [CatsService],
+})
+export class AppModule { }
